bb3b25ada8950ee2e1fd6eee5f6ca11f8f10c4ce
Juan Fernando <fernandojuan.dev@gmail.com>
2025-06-30 18:23:32 -0300

    Ajuste de fonte e adição de javascript para efeitos visuais (fade in).

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    document.querySelectorAll('.section_container').forEach(section => {
+        observer.observe(section);
+    });
+});
